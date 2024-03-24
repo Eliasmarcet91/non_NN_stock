c61c6341b2f0d219f08051942162ddc7bac62e59
Eliasmarcet91 <73188124+Eliasmarcet91@users.noreply.github.com>
2024-03-24 11:37:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,4 @@
-import subprocess
-import sys
-
-# Function to install packages
-def install_packages(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# Install Plotly
-install_packages("plotly")
-
-
+pip install plotly
 import streamlit as st
 import requests
 import pandas as pd
